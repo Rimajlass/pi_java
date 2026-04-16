32e337afa47c3ee6470673745fc84ab4b3c84738
Youss-99 <youssef.rabai@esprit.tn>
2026-04-16 03:12:51 +0100

    Fix module-info.java to include investment module packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module Pidev3A47 {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.j;
+    requires com.fasterxml.jackson.databind;
 
     exports pi.mains;
     exports pi.savings.ui;
@@ -18,4 +19,8 @@ module Pidev3A47 {
     opens pi.controllers.ImprevusCasreelController to javafx.fxml;
     opens pi.controllers.CoursQuizController to javafx.fxml;
     opens pi.savings.ui to javafx.fxml;
+    opens pi.controllers.InvestissementController to javafx.fxml;
+    opens pi.entities to javafx.fxml;
+    opens pi.services.InvestissementService to javafx.fxml;
+    opens pi.savings.repository to javafx.fxml;
 }
